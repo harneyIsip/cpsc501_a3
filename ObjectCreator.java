2e5036057d2b5b66c6793fa71b40026cfcf5d183
Harney Isip <harney_isip@yahoo.com>
2019-11-15 10:08:41 -0700

    creation of object with only primitives

--- ObjectCreator.java
+++ ObjectCreator.java
@@ -33,25 +33,48 @@ public class ObjectCreator {
 		else if(input.equals("5")){
 			object = createObjectCollection();
 		}
+		else{
+			System.out.println("Only select from 1-5");
+		}
 		return object;
 	}
-
+	/*
+	* Simple object with only primitives for instance variables
+	 */
 	public Object1 createSimplePrimitive(){
+		int prim1,prim2;
+		System.out.println("Enter an integer: ");
+		prim1 = scan.nextInt();
+		System.out.println("Enter another integer");
+		prim2 = scan.nextInt();
 
+		return new Object1(prim1,prim2);
 	}
 
+	/*
+	 * Object that contains references to other objects
+	 */
 	public Object2 createReferenceObject(){
 
 	}
 
+	/*
+	 * Object that contains an array or primitives
+	 */
 	public Object3 createArrayPrimitives(){
 
 	}
 
+	/*
+	 * Object that contains an array of object references
+	 */
 	public Object4 createArrayObject(){
 
 	}
 
+	/*
+	 * Object that uses an instance of Java's collection classes
+	 */
 	public Object5 createObjectCollection(){
 
 	}
